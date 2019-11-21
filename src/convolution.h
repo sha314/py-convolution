0bc9abea89b9810109e45bc5520fdc3ef6d6e411
sha314 <shahnoor3pl@gmail.com>
2019-11-21 22:59:18 +0600

    fast convolution cpp code is added

--- src/convolution.h
+++ src/convolution.h
@@ -18,4 +18,17 @@ void convolve2d(
 	double **data_out, int *dim1_out, int *dim2_out);
 
 
+// for 1d array
+void convolve1d_fast(
+    double *data, int columns,
+    int thread_count,
+    double **data_out, int *dim_out, double threshold=1e-15);
+
+// for 2d array
+void convolve2d_fast(
+	double *data_in, int n_rows, int n_columns,
+	int thread_count,
+	double **data_out, int *dim1_out, int *dim2_out, double threshold=1e-15);
+
+
 #endif // WORD_H_ 
--- test.py
+++ test.py
@@ -6,35 +6,39 @@ import numpy as np
 # help(stm)
 # help(stm.convolve2d)
 
-a = np.linspace(0, 1, 20)
+# a = np.linspace(0, 1, 20)
 
-print("checking for 1d array-------------------------------------")
-print (a.shape)
-c = stm.convolve1d(a, 1)
-print (c)
+# print("checking for 1d array-------------------------------------")
+# print (a.shape)
+# c = stm.convolve1d(a, 1)
+# print (c)
 
-print("checking for 2d array-------------------------------------")
-a = a.reshape((a.shape[0], 1))
-print (a.shape)
-print(a)
-c = stm.convolve2d(a, 1)
-print (c)
+# print("checking for 2d array-------------------------------------")
+# a = a.reshape((a.shape[0], 1))
+# print (a.shape)
+# print(a)
+# c = stm.convolve2d(a, 1)
+# print (c)
 
 
 
-# print("testing a file")
-# import matplotlib.pyplot as plt
+print("testing a file")
+import matplotlib.pyplot as plt
 
-# filename = "sq_lattice_site_percolation_periodic_entropy_avg_std-L_200.csv"
-# x, y = np.loadtxt(filename, usecols=(0,1), unpack=True)
+filename = "sq_lattice_site_percolation_periodic_entropy_avg_std-L_200.csv"
+x, y = np.loadtxt(filename, usecols=(0,1), unpack=True)
 # plt.plot(x, y, label='old')
 
-# y1 = stm.convolve1d(y, 3)
+
+y1 = stm.convolve1d(y, 4)
 # plt.plot(x, y1, label='new')
 # plt.ylim(0, 13)
 # plt.legend()
 # plt.show()
 
-# C = -(1-x[1:]) * np.diff(y1) / np.diff(x)
-# plt.plot(x[1:], C)
-# plt.show()
+C = -(1-x[1:]) * np.diff(y) / np.diff(x)
+plt.plot(x[1:], C)
+
+C = -(1-x[1:]) * np.diff(y1) / np.diff(x)
+plt.plot(x[1:], C)
+plt.show()
